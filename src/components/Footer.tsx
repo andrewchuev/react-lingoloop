468832d43ce35c3a8b26daea78a1217009392245
Andrew A. Chuev <andrew.chuev@gmail.com>
2026-03-20 18:22:15 +0200

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
     return <>
-        <div className="mx-auto my-8">© 2026 <a href="https://reslab.pro">Chuev Research.</a> All rights reserved.</div>
+        <div className="mx-auto my-2">© 2026 <a href="https://reslab.pro">Chuev Research.</a> All rights reserved.</div>
     </>;
 }
